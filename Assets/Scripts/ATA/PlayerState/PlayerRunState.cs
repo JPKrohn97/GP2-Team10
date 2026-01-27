692bf536af47b693f48ff37d41e0f682750d2988
alpbln <alpbilen@gmail.com>
2026-01-27 12:18:48 +0100

    a

--- Assets/Scripts/ATA/PlayerState/PlayerRunState.cs
+++ Assets/Scripts/ATA/PlayerState/PlayerRunState.cs
@@ -1,30 +1,29 @@
 using UnityEngine;
 
-
-public class PlayerRunState : PlayerGroundedState 
+public class PlayerRunState : PlayerGroundedState
 {
+
     private const float DeadZone = 0.01f;
+
     public PlayerRunState(PlayerController player, PlayerStateMachine stateMachine) : base(player, stateMachine) { }
 
     public override void Enter()
     {
         base.Enter();
-
-        if (player.AnimationEvents != null)
-            player.AnimationEvents.SetMovingBool(true);
+        player.AnimationEvents.SetMovingBool(true);
     }
 
     public override void Exit()
     {
         base.Exit();
-
-        if (player.AnimationEvents != null)
-            player.AnimationEvents.SetMovingBool(false);
+        player.AnimationEvents.SetMovingBool(false);
     }
 
     public override void LogicUpdate()
     {
+
         base.LogicUpdate();
+
 
         if (stateMachine.CurrentState != this) return;
 
@@ -33,13 +32,6 @@ public class PlayerRunState : PlayerGroundedState
         {
             stateMachine.ChangeState(player.IdleState);
         }
-        
-        /*
-        if (player.InputHandler.Player.Jump.triggered)
-        {
-            stateMachine.ChangeState(player.JumpState);
-        }
-        */
     }
 
     public override void PhysicsUpdate()
@@ -49,18 +41,16 @@ public class PlayerRunState : PlayerGroundedState
         float inputX = player.CurrentMovementInput.x;
 
 
-        Vector3 velocity = player.RB.linearVelocity;
-        
+        Vector3 v = player.RB.linearVelocity;
+        v.z = inputX * player.moveSpeed;
+        v.x = 0f; 
+        player.RB.linearVelocity = v;
 
-        velocity.z = inputX * player.moveSpeed; 
-        velocity.x = 0f; 
-        player.RB.linearVelocity = velocity;
-        
+
         if (Mathf.Abs(inputX) > DeadZone)
         {
 
             float targetY = (inputX > 0f) ? 0f : 180f; 
-
             player.transform.rotation = Quaternion.Euler(0f, targetY, 0f);
         }
     }
