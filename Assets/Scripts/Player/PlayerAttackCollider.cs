6102252ad7449fb47844cc3861c5f7e51683f351
aniltahmaz <anil.tahmaz@gmail.com>
2026-01-27 15:38:02 +0100

    +player can damage enemies

---
+++ Assets/Scripts/Player/PlayerAttackCollider.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerAttackCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+
+            enemy.TakeDamage(35);
+
+        }
+    }
+}
