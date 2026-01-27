f574d676241b37998dadaf29b06b04c05639c2e7
alpbln <alpbilen@gmail.com>
2026-01-27 15:23:31 +0100

    a

---
+++ Assets/ithappy/Adventure_Land/Scripts/Demonstration/Character/CroakLand.cs
@@ -0,0 +1,22 @@
+namespace ithappy.Adventure_Land
+{
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CroakLand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+}
