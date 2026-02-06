9f4e7e3f212c3232527aae43358899b4dac87b50
DESKTOP-37K31B3\ATA <sahinhakan3506@gmail.com>
2026-02-06 13:23:58 +0300

    jump animation replaced

--- Assets/Scripts/ATA/Controller/MobileTouchController.cs
+++ Assets/Scripts/ATA/Controller/MobileTouchController.cs
@@ -1,17 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.UI;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class MobileTouchController : MonoBehaviour
 {
     public UnityEvent onPress;
+    
+    [Header("Renk Ayarları")]
+    public Color pressedColor = Color.gray; // Basıldığında olacak renk
+    private Color originalColor;            // Butonun orijinal rengi
+    private Image myImage;
 
     private RectTransform myRect;
+    private int pressedFingerId = -1;
 
     private void Awake()
     {
         myRect = GetComponent<RectTransform>();
+        myImage = GetComponent<Image>();
+
+        // Eğer objede bir Image varsa, başlangıç rengini hafızaya al
+        if (myImage != null)
+        {
+            originalColor = myImage.color;
+        }
     }
 
     private void OnEnable()
@@ -24,14 +38,31 @@ public class MobileTouchController : MonoBehaviour
 
         foreach (var touch in Touch.activeTouches)
         {
-
+            // --- 1. DOKUNMA BAŞLADI (BEGAN) ---
             if (touch.phase == UnityEngine.InputSystem.TouchPhase.Began)
             {
-
-                if (RectTransformUtility.RectangleContainsScreenPoint(myRect, touch.screenPosition))
+                // Eğer butona şu an kimse basmıyorsa (pressedFingerId == -1) 
+                // VE dokunma butonun üzerindeyse:
+                if (pressedFingerId == -1 && RectTransformUtility.RectangleContainsScreenPoint(myRect, touch.screenPosition))
                 {
-                    onPress.Invoke();
+                    pressedFingerId = touch.finger.index; // Bu parmağı kaydet
+                    
+                    // Rengi Değiştir (Basıldı Rengi)
+                    if (myImage != null) myImage.color = pressedColor;
+
+                    onPress.Invoke(); // Fonksiyonu çalıştır (Zıplama vb.)
                 }
+            }
+
+            // --- 2. DOKUNMA BİTTİ (ENDED veya CANCELED) ---
+            // Sadece bizim butona basan parmak (pressedFingerId) kalktıysa işlem yap
+            if (touch.finger.index == pressedFingerId && 
+                (touch.phase == UnityEngine.InputSystem.TouchPhase.Ended || touch.phase == UnityEngine.InputSystem.TouchPhase.Canceled))
+            {
+                // Rengi Eski Haline Getir
+                if (myImage != null) myImage.color = originalColor;
+
+                pressedFingerId = -1; // Butonu serbest bırak, artık basılmıyor
             }
         }
     }
