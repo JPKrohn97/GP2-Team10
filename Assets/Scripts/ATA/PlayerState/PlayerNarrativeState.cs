7c75c2adeb839ed2209044b062967b15580acca2
JPKrohn97 <54819241+JPKrohn97@users.noreply.github.com>
2026-02-11 13:13:44 +0100

    Pushing cinematic narrative

---
+++ Assets/Scripts/ATA/PlayerState/PlayerNarrativeState.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using DG.Tweening; 
+
+public class PlayerNarrativeState : PlayerState
+{
+    //private float biteDuration = 2f;     
+    //private float mutationDuration = 2.0f; 
+    //private Tween mutationTween;
+
+    private bool movementLocked = true; 
+
+    public PlayerNarrativeState(PlayerController player, PlayerStateMachine stateMachine) 
+        : base(player, stateMachine) { }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        movementLocked = true;
+        player.RB.linearVelocity = Vector3.zero;
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+
+        if (movementLocked)
+        {
+            player.RB.linearVelocity = new Vector3(0, player.RB.linearVelocity.y, 0);
+        }
+    }
+
+    private void FinishNarrative()
+    {
+        movementLocked = false; 
+        stateMachine.ChangeState(player.IdleState);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        movementLocked = false;
+    }
+}
